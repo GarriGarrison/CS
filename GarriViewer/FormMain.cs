2d92dbf3f5e16ca9f629c46b64de72adda684f4c
Igor Ermakov <77785633+GarriGarrison@users.noreply.github.com>
2021-12-20 19:49:07 +0300

    Версия 1.0

---
+++ GarriViewer/FormMain.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GarriViewer
+{
+    public partial class MAinForm : Form
+    {
+        public MAinForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
